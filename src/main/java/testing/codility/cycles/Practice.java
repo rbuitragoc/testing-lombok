9da583ed8f641117e315c4e1d8256c8e4549328d
Rick Buitrago <rbuitragoc@gmail.com>
2017-11-17 14:54:50 -0500

    added documentation, added rectangle triangle tests, without streams

--- src/main/java/testing/codility/cycles/Practice.java
+++ src/main/java/testing/codility/cycles/Practice.java
@@ -8,12 +8,38 @@ import java.util.stream.IntStream;
 public class Practice {
 
     public static void main(String ... args) {
+        Practice p = new Practice();
         int input = 10;
-        System.out.println(String.format("Factorial of %s (%s!) using parallel streams: %s", input, input,
-                new Practice().factorial(input)));
+        System.out.println(String.format("Factorial of %s (%s!) using parallel streams: %s\n", input, input,
+                p.factorial(input)));
+        System.out.println(String.format("Rectangle Triangle of base %s: \n %s", input, p.rectangleTriangle(input)));
+
     }
 
+    /**
+     * Pretty self explanatory
+     * @param n is the number for which we want to calculate factorial function
+     * @return factorial function calculated on given n
+     * @throws ArithmeticException if at any step of mutiplying n * (n+1) produces a number larger than Integer.MAX_VALUE
+     */
     private int factorial(int n) {
         return IntStream.rangeClosed(1, n).parallel().reduce(1, Math::multiplyExact);
+    }
+
+    /**
+     * Prints a rectangle triangle with the 90 degree angle at the bottom line, thus making legs parallel and orthogonal
+     * to the horizon, correspondingly. The base leg measures exactly n asterisks.
+     * The horizontal distance between any pair of asterisks is one whitespace character.
+     * @param n the size of the base leg of the triangle
+     */
+    private String rectangleTriangle(int n) {
+        StringBuilder buf = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            for (int x = 1; x < i; x++) {
+                buf.append("* ");
+            }
+            buf.append("\n");
+        }
+        return buf.toString();
     }
 }
