d722a3ff515ea238c6352edd68342dac9067901b
Rick Buitrago <rbuitragoc@gmail.com>
2017-11-20 10:50:12 -0500

    added upside down triangle without streams

--- src/main/java/testing/codility/cycles/Practice.java
+++ src/main/java/testing/codility/cycles/Practice.java
@@ -9,10 +9,11 @@ public class Practice {
 
     public static void main(String ... args) {
         Practice p = new Practice();
-        int input = 10;
+        int input = 4;
         System.out.println(String.format("Factorial of %s (%s!) using parallel streams: %s\n", input, input,
                 p.factorial(input)));
-        System.out.println(String.format("Rectangle Triangle of base %s: \n %s", input, p.rectangleTriangle(input)));
+        System.out.println(String.format("Rectangle Triangle of base %s:%s", input, p.rectangleTriangle(input)));
+        System.out.println(String.format("Upside down Triangle of base %s: \n\n %s", input, p.upsideDownTriangle(input)));
 
     }
 
@@ -38,6 +39,28 @@ public class Practice {
             for (int x = 1; x < i; x++) {
                 buf.append("* ");
             }
+            buf.append("\n");
+        }
+        return buf.toString();
+    }
+
+    /**
+     * Print a triangle made of asterisks separated by spaces and consisting of n rows again,
+     * but this time upside down, and make it symmetrical. Consecutive rows have 2n - 1, 2n - 3, ..., 3, 1 asterisks
+     * and should be indented by o, 2, 4, ..., 2(n - 1) spaces.
+     * @param n
+     * @return
+     */
+    private String upsideDownTriangle(int n) {
+        StringBuilder buf = new StringBuilder();
+        for (int i = n; i > 0; i--) {
+            for (int s = 1; s <= (n - i); s++) {
+                buf.append("  ");
+            }
+            for (int a = 1; a < 2 * (i); a++) {
+                buf.append("* ");
+            }
+
             buf.append("\n");
         }
         return buf.toString();
