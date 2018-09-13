fbbe2a0bea6ce86a82043df00c248b78dcf275f7
Rick Buitrago <rbuitragoc@gmail.com>
2018-09-13 08:54:44 -0500

    added @MethodSource for externalizing test source

--- src/test/java/testing/palindrome/SolutionTest.java
+++ src/test/java/testing/palindrome/SolutionTest.java
@@ -2,9 +2,14 @@ package testing.palindrome;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class SolutionTest {
   
@@ -17,15 +22,25 @@ class SolutionTest {
   
   @ParameterizedTest
   @ValueSource(strings = {"anita lava la tina", "dabale arroz a la zorra el abad", "axxa"})
-  void checkPalindrome(String word) {
-    boolean actualResult = underTest.checkPalindrome(word);
-    assertTrue(actualResult);
+  void checkSpanishPalindrome(String palabra) {
+    assertTrue(underTest.checkPalindrome(palabra));
+  }
+  
+  @ParameterizedTest
+  @MethodSource("getEnglishPalindromes")
+  void checkEnglishPalindrome(String word) {
+    assertTrue(underTest.checkPalindrome(word));
   }
   
   @ParameterizedTest
   @ValueSource(strings = {"any phrase that's not a palindrome", "longword", "or just a short one"})
   void checkNonPalindrome(String word) {
-    boolean actualResult = underTest.checkPalindrome(word);
-    assertFalse(actualResult);
+    assertFalse(underTest.checkPalindrome(word));
   }
+  
+  // source factory method
+  private static Stream<Arguments> getEnglishPalindromes() {
+    return Stream.of(Arguments.of("racecar"), Arguments.of("radar"), Arguments.of("able was I ere I saw elba"));
+  }
+  
 }
