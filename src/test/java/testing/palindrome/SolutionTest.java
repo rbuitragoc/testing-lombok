6db3aa3a515bea21345bd74f439e0c17d535978e
Rick Buitrago <rbuitragoc@gmail.com>
2018-09-13 09:10:55 -0500

    replaced for @CSVSource

--- src/test/java/testing/palindrome/SolutionTest.java
+++ src/test/java/testing/palindrome/SolutionTest.java
@@ -3,6 +3,7 @@ package testing.palindrome;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -21,7 +22,7 @@ class SolutionTest {
   }
   
   @ParameterizedTest
-  @ValueSource(strings = {"anita lava la tina", "dabale arroz a la zorra el abad", "axxa"})
+  @ValueSource(strings = {"anita lava la tina", "dabale arroz a la zorra el abad", "amar a la rama"})
   void checkSpanishPalindrome(String palabra) {
     assertTrue(underTest.checkPalindrome(palabra));
   }
@@ -33,7 +34,7 @@ class SolutionTest {
   }
   
   @ParameterizedTest
-  @ValueSource(strings = {"any phrase that's not a palindrome", "longword", "or just a short one"})
+  @CsvSource({"anything", "realize", "40912"})
   void checkNonPalindrome(String word) {
     assertFalse(underTest.checkPalindrome(word));
   }
